23996c601df7a377d519da694f17e329c94be64c
li <34814838+cha2r@users.noreply.github.com>
2019-05-22 16:05:52 +0800

    Update device.py

--- modules/device.py
+++ modules/device.py
@@ -1,15 +1,4 @@
 #coding=utf-8
-def device_filter(dev_content):
-    """ dev_content显示了设备的名称和信息
-        这里通过关键字查找的方式来判断该设备是否是键盘设备
-    """
-    # 如果设备信息出现中出现了keyboard这个关键词，那么就认为是键盘设备
-    print "device info: ", dev_content
-    if "keyboard" in dev_content.lower():
-        return True
-    return False
-
-
 def find_keyboard_devices(device_filter_func):
     """
         找出所有的键盘设备名
@@ -28,13 +17,4 @@ def find_keyboard_devices(device_filter_func):
         print("没有键盘设备")
         # 直接结束该进程
         sys.exit(-1)
-    return result
-
-
-def monitor_keyboard(devs):
-
-    # 将名映射到inputDevice对象
-    devices = map(InputDevice, devs)
-    # dev.fd一个文件描述符， 然后建立一个字典
-    devices = {dev.fd: dev for dev in devices}
-    return devices
+    return str(result)
