5badbe5066ea1b6d3c89ce3379f97a057dd9f7e5
li <34814838+cha2r@users.noreply.github.com>
2019-05-28 16:14:56 +0800

    Update port.py

--- modules/port.py
+++ modules/port.py
@@ -8,8 +8,9 @@ def run(**args):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex(('127.0.0.1', index))
         if result == 0:
-            result = "Port %d is open" % index
+            temp = "Port %d is open" % index
         else:
-            result = "Port %d is not open" % index
+            temp = "Port %d is not open" % index
         sock.close()
-    return str(result)
+    return str(temp)
+
