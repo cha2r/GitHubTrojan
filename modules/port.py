932ef9811fa519b79eb825f99407a8bd08351b5e
li <34814838+cha2r@users.noreply.github.com>
2019-05-29 17:08:55 +0800

    Create port.py

---
+++ modules/port.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+import socket
+
+port_number = [135,443,80]
+def run(**args):
+    for index in port_number:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex(('127.0.0.1', index))
+        if result == 0:
+                file  = "Port %d is open\n" % index
+        else:
+                file = "Port %d is not open\n" % index
+        temp = temp.append(file)
+        
+        sock.close()
+    
+    return str(temp)
