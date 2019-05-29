1ead14cbdf83ace001ca9aea204ddfdcbe67c211
li <34814838+cha2r@users.noreply.github.com>
2019-05-29 18:07:06 +0800

    Update port.py

--- modules/port.py
+++ modules/port.py
@@ -4,6 +4,7 @@ import socket
 port_number = [135,443,80,8080,21,23,22,110]
 def run(**args):
     temp=[]
+    print "[*]Now in port module."
     for index in port_number:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex(('127.0.0.1', index))
