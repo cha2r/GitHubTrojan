df23358ca5aafa3a830a947a149e3e16266187fc
li <34814838+cha2r@users.noreply.github.com>
2019-05-29 18:06:33 +0800

    Update port.py

--- modules/port.py
+++ modules/port.py
@@ -1,16 +1,16 @@
 #coding=utf-8
 import socket
 
-port_number = [135,443,80]
+port_number = [135,443,80,8080,21,23,22,110]
 def run(**args):
     temp=[]
     for index in port_number:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex(('127.0.0.1', index))
         if result == 0:
-                file  = "Port %d is open\n" % index
+                file  = "Port %d is open" % index
         else:
-                file = "Port %d is not open\n" % index
+                file = "Port %d is not open" % index
         temp.append(file)
         
         sock.close()
