203c295d8dc157eda35be41ac2482895c4fa554a
li <34814838+cha2r@users.noreply.github.com>
2019-06-05 10:12:43 +0800

    Create dmidecode.py

---
+++ modules/dmidecode.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+
+from  subprocess  import  Popen, PIPE
+import os 
+
+def  run(**args):
+    print "[*]Now in dmidecode module."
+    p = Popen(['dmidecode'], stdout=PIPE, stderr=PIPE)
+    data = p.stdout.read()
+    return str(data)
