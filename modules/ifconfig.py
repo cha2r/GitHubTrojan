b6974ac51fa3e8257ec417521990a65b152d2f4b
li <34814838+cha2r@users.noreply.github.com>
2019-06-05 12:52:26 +0800

    Update ifconfig.py

--- modules/ifconfig.py
+++ modules/ifconfig.py
@@ -2,8 +2,8 @@
 
 from  subprocess  import  Popen, PIPE
 
-def  run(**args):
-        print "[*]Now in ifconfig module."
+def run(**args):
+    print "[*]Now in ifconfig module."
     p = Popen(['ifconfig'], stdout=PIPE, stderr=PIPE)
     data = p.stdout.read()
     return str(data)
